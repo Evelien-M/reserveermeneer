76d4ef4caea1a30792780e75fa47657648271aee
Evelien-M <32199993+Evelien-M@users.noreply.github.com>
2021-12-13 14:35:31 +0100

    seed added

--- database/seeders/EventsTable.php
+++ database/seeders/EventsTable.php
@@ -74,7 +74,67 @@ class EventsTable extends Seeder
                 "max_amount_tickets_per_person" => 5,
                 "location" => "Landgraaf",
                 "price" => 19.99
-            ]
+            ],
+            [
+                "name" => "Paaspop",
+                "image" => "123242.png",
+                "content" => "Paaspop is een driedaags muziekfestival dat elk jaar tijdens het paasweekend plaatsvindt in het Nederlandse Schijndel (Noord-Brabant).",
+                "content2" => "Paaspop is a three-day music festival that takes place every year during the Easter weekend in Schijndel (North Brabant) in the Netherlands.",
+                "event_start_date" => "2022-09-06 15:00:00",
+                "event_end_date" => "2022-09-09 20:00:00",
+                "max_amount_tickets" => 500,
+                "max_amount_tickets_per_person" => 5,
+                "location" => "Schijndel",
+                "price" => 9.99
+            ],
+            [
+                "name" => "Aardappelpop",
+                "image" => "test2.png",
+                "content" => "Lorem ipsum dolor sit amet.",
+                "content2" => "Lorem ipsum dolor sit amet.",
+                "event_start_date" => "2022-10-06 15:00:00",
+                "event_end_date" => "2022-10-09 20:00:00",
+                "max_amount_tickets" => 100,
+                "max_amount_tickets_per_person" => 15,
+                "location" => "Tilburg",
+                "price" => 9.99
+            ],
+            [
+                "name" => "Reading Festival",
+                "image" => "1234453.jpg",
+                "content" => "De Reading en Leeds festivals zijn een paar jaarlijks terugkerende muziekfestivals in de Engelse steden Reading en Leeds die meestal plaatsvinden in augustus. Vaak hebben de festivals dezelfde entreeprijs[bron?], maar er zijn uitzonderingen bekend. Tussen 1998 en 2007 heetten de festivals het Carling Weekend totdat er een breuk kwam met de sponsor Carling in november 2007.",
+                "content2" => "The Reading and Leeds festivals are a pair of annual music festivals in the English cities of Reading and Leeds that usually take place in August. The festivals often have the same entrance fee [source?], but there are known exceptions. Between 1998 and 2007 the festivals were called the Carling Weekend until their sponsor Carling split in November 2007.",
+                "event_start_date" => "2022-8-12 15:00:00",
+                "event_end_date" => "2022-8-15 20:00:00",
+                "max_amount_tickets" => 1000,
+                "max_amount_tickets_per_person" => 15,
+                "location" => "London",
+                "price" => 19.99
+            ],
+            [
+                "name" => "Appelpop",
+                "image" => "13241221.jpg",
+                "content" => "Appelpop is een tweedaags popfestival dat jaarlijks in Tiel, in de Nederlandse provincie Gelderland, georganiseerd wordt door Stichting Muziekstad Tiel (SMT). Het is het grootste gratis muziekfestival van Nederland en vindt telkens in het tweede weekend van september op vrijdag en zaterdag plaats op de Waalkade.",
+                "content2" => "Appelpop is a two-day pop festival that is organized annually in Tiel, in the Dutch province of Gelderland, by Stichting Muziekstad Tiel (SMT). It is the largest free music festival in the Netherlands and takes place every Friday and Saturday on the Waalkade in the second weekend of September.",
+                "event_start_date" => "2022-5-12 15:00:00",
+                "event_end_date" => "2022-5-14 20:00:00",
+                "max_amount_tickets" => 800,
+                "max_amount_tickets_per_person" => 8,
+                "location" => "London",
+                "price" => 19.99
+            ],
+            [
+                "name" => "Aardappelpop V2",
+                "image" => "1123.png",
+                "content" => "Lorem ipsum dolor sit amet 2.",
+                "content2" => "Lorem ipsum dolor sit amet 2.",
+                "event_start_date" => "2022-10-16 15:00:00",
+                "event_end_date" => "2022-10-19 20:00:00",
+                "max_amount_tickets" => 50,
+                "max_amount_tickets_per_person" => 25,
+                "location" => "Tilburg",
+                "price" => 9.99
+            ],
         ];
 
         foreach ($events as $event)
